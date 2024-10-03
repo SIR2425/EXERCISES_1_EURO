59dea8b88576a73958872470b7baac0aeeccc2e7
Pedro Miguel do Vale Moreira <2411672+pedromoreira-estg@users.noreply.github.com>
2024-10-03 12:12:09 +0100

    init v1.2 PL3

--- PL3 V.1.1/euro.js
+++ PL3 V.1.1/euro.js
@@ -3,16 +3,66 @@ document.addEventListener('DOMContentLoaded', (e) => {
     button = document.getElementById("genBtn");
 
     button.addEventListener('click', (e) => {
-        //alert("You Clicked Me!");
+        genNewBet();
     });
 
+    /*
     button.addEventListener('click', function (e) {
         console.log("same event, another handle");
     });
-
-    button.addEventListener('click', addtext);
+     button.addEventListener('click', addtext);
+    */
 });
 
+
+function genRandomNumbers(n, min, max) {
+    // return an array with n unique integers between min and max
+    let setOfNumbers = new Set();
+    while (setOfNumbers.size < n) {
+        newNumber = Math.floor(Math.random() * (max - min + 1) + min);
+        setOfNumbers.add(newNumber);
+    }
+    return [...setOfNumbers].sort((a,b) => a-b);
+}
+
+function genNewBet() {
+
+    numbers = genRandomNumbers(5, 1, 50);
+    stars = genRandomNumbers(2, 1, 12);
+
+    console.log(numbers, stars);
+   
+    theOLNumbers = document.getElementById('olMain');
+    theOLNumbers.innerHTML = "";
+
+    numbers.forEach(number => {
+        newLi = document.createElement("li");
+        newLi.innerHTML = number;
+        theOLNumbers.appendChild(newLi);
+    });
+
+    theOLStars = document.getElementById('olStars');
+    theOLStars.innerHTML = "";
+
+    stars.forEach(star => {
+        newLi = document.createElement("li");
+        newLi.innerHTML = star;
+        theOLStars.appendChild(newLi);
+    });
+
+    // for each number
+        // create a new li with the number
+        // add it to the list of numbers
+    
+    // do the same for the stars
+
+
+}
+
+
+
+
+/*
 function addtext() {
     listofnumbers = document.getElementById("olMain");
     listofnumbers.innerHTML = "";
@@ -22,6 +72,7 @@ function addtext() {
 
     listofnumbers.appendChild(newli);
 }
+*/
 
 // Math.random() 0..1;
 // Math.floor() turn a real into an integer
