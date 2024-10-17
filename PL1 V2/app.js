2a984b26894d422973a894b2e7890d9985711828
Pedro Miguel do Vale Moreira <2411672+pedromoreira-estg@users.noreply.github.com>
2024-10-17 09:40:19 +0100

    package

--- PL1 V2/app.js
+++ PL1 V2/app.js
@@ -1,5 +1,3 @@
-//
-
 express = require('express');
 
 app = new express();
@@ -11,6 +9,6 @@ app.get('/', function(req, res) {
 
 app.get('/euro', function (req, res) {
     res.set("content-type", 'application/json');
-    res.send('{numeros:[1,2,3,4,5,6], estrelas:[1,2]}');
+    res.send('{"numeros":[1,2,3,4,5,6], "estrelas":[1,2]}');
 });
 app.listen(3000);
